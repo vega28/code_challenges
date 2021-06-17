bc4ea3352fc0b78b9d1bdae9a31c3ff237cec1b3
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-06-17 13:32:19 -0700

    Minor refactor for readability.

--- codewars/is_prime.py
+++ codewars/is_prime.py
@@ -7,8 +7,12 @@
 
 # Constraints:
 # You can assume you will be given an integer input.
-# You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
-# NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 (or similar, depends on language version). Looping all the way up to n, or n/2, will be too slow.
+# You can not assume that the integer will be only positive. 
+#   You may be given negative numbers as well (or 0).
+# NOTE on performance: There are no fancy optimizations required, but 
+#   still the most trivial solutions might time out. Numbers go up to 2^31 
+#   (or similar, depends on language version). Looping all the way up to n, 
+#   or n/2, will be too slow.
 
 # Code:
 
